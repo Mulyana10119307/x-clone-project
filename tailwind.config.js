ec59505fd81b354d4e5525e5642a8d0d4a17b7ef
Mulyana <mulcoding@gmail.com>
2024-01-08 20:53:52 +0700

    Menambahkan icon svg, edit index.html dan style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html"],
   theme: {
     extend: {},
+    screens: {
+      mobile: "370px",
+      tablet: "640px",
+      laptop: "1024px",
+      dekstop: "1280px",
+    },
   },
   plugins: [],
-}
-
+};
