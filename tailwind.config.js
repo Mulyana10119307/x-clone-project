b608216697f052e4fd92865c8775f3cfbe68d774
Mulyana <mulcoding@gmail.com>
2024-01-11 12:51:45 +0700

    Menambahkan pages dan scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js,css}",
+    "./scripts/**/*.{html,js,css}",
+  ],
   theme: {
     extend: {},
     screens: {
